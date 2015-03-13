327932de61f932cda41ccb57f2fa3c17520fdf3f
Jake Marwil <rjm02006@gmail.com>
2015-03-13 14:11:47 -0600

    add get route for facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'reddit_fetcher#index'
+  get '/auth/:provider/callback' => 'secrets#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
