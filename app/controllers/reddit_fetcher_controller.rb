9dc2079044f70f5c6b1530fb77b17b791b261578
Jake Marwil <rjm02006@gmail.com>
2015-03-13 13:43:10 -0600

    add @search and @comments to index method

--- app/controllers/reddit_fetcher_controller.rb
+++ app/controllers/reddit_fetcher_controller.rb
@@ -1,7 +1,9 @@
 class RedditFetcherController < ApplicationController
 
   def index
-    
+    reddit_fetcher = RedditFetcher.new
+    @search_key = reddit_fetcher.search_key
+    @comments = reddit_fetcher.comments
   end
 
 end
